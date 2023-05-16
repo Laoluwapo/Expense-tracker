36e6248d44fb47dc798d1ab74be99427715a94ed
laoluwapo <alividegabriel9@gmail.com>
2023-05-16 16:14:26 +0100

    Create an expense list with dummy data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import ExpenseList from "./components/ExpenseList";
 function App() {
-  return <div></div>;
+  // Dummy array of expenses
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Savings", amount: 50, category: "December balling" },
+    { id: 2, description: "bbb", amount: 50, category: "December balling" },
+    { id: 3, description: "ccc", amount: 50, category: "December balling" },
+    { id: 4, description: "ddd", amount: 50, category: "December balling" },
+  ]);
+
+  return (
+    <div>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </div>
+  );
 }
 
 export default App;
